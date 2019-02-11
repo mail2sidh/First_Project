eef99e883209cf2a3bf654962ccc87cabcf841c4
sidh <sidh14319@gmail.com>
2019-02-12 01:16:47 +0530

    This is updated cpp file in develop branch

--- src/trial.cpp
+++ src/trial.cpp
@@ -3,6 +3,7 @@ using namespace std
 
 int main()
 { 
-	cout << "The change is done in Master 3rd commit it is ." ; 
+	cout << "The change is done in Master 3rd commit it is ." ;
+	cout << "The change is now in Dev branch . "
 	return 0 ;
 }
