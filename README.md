c65c3498a2ab62917d80a86768b52df37d942ba0
sidh <sidh14319@gmail.com>
2019-02-12 01:32:02 +0530

    This is to push from different branch

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ This is readme file for my 1st project.
 
 This is the project that will run a cpp file. 
 It will put code in src file.
-
+Here the update is in develop but to create conflict.
 After pushing to repo this is edit in 4th commit.
--- src/trial.cpp
+++ src/trial.cpp
@@ -4,6 +4,6 @@ using namespace std
 int main()
 { 
 	cout << "The change is done in Master 3rd commit it is ." ;
-	cout << "The change is now in Dev branch . "
+	cout << "The change is now in Dev branch with the merge just to test. "
 	return 0 ;
 }
