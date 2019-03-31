02ed9c7199bf5a057d573668258751a6a73f6b8c
mail2sidh <33795979+mail2sidh@users.noreply.github.com>
2019-03-31 22:05:48 +0530

    Create merge conflicts

--- src/trial.cpp
+++ src/trial.cpp
@@ -5,5 +5,6 @@ int main()
 { 
 	cout << "The change is done in Master 3rd commit it is ." ;
 	cout << "The change is now in Dev branch . "
+	cout << "This is the try to create merge conflict.";	
 	return 0 ;
 }
